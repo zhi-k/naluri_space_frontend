16db6023437bfd4a0e28d393e6c3d289ac0644f4
zhi-k <chongz.kang@gmail.com>
2022-06-02 12:27:05 +0800

    chore: add windicss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), WindiCSS()],
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "./src"),
