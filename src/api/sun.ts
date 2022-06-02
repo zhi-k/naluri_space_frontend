1c4d918f57075f75a6165fb3652e4b0f63f185fe
zhi-k <chongz.kang@gmail.com>
2022-06-02 18:09:47 +0800

    feat(sun): use api data

--- src/api/sun.ts
+++ src/api/sun.ts
@@ -1,5 +1,16 @@
 import client from '@/services/naluri/client';
 
-export function getCircumference() {
-	return client.get("/api/v1/sun");
+export interface ISunCircumferenceData {
+	success: boolean;
+	message: string;
+	data: {
+		circumference: string;
+		pi: string;
+		elapsed_time: number;
+		decimal_points: number;
+	}
+}
+
+export async function getCircumference() {
+	return await client.get("/api/v1/sun");
 }
--- src/pages/Sun/index.tsx
+++ src/pages/Sun/index.tsx
@@ -1,15 +1,26 @@
-import CircumferenceLayout from "@/layouts/CircumferenceLayout";
-import { getCircumference } from "@/api/sun";
+import { useState, useEffect } from "react";
 
-const fakeData = {
-	circumference: "20",
-	pi: "50",
-	elapsedTime: "500",
-	decimalPoints: 10,
-};
+import CircumferenceLayout from "@/layouts/CircumferenceLayout";
+import { getCircumference, ISunCircumferenceData } from "@/api/sun";
 
 export default function Sun() {
+	const [data, setData] = useState({
+		circumference: "0",
+		pi: "0",
+		elapsedTime: 0,
+		decimalPoints: 0,
+	});
 
+	useEffect(() => {
+		getCircumference().then(({ data }: { data: ISunCircumferenceData }) => {
+			setData({
+				circumference: data.data.circumference,
+				pi: data.data.pi,
+				elapsedTime: data.data.elapsed_time,
+				decimalPoints: data.data.decimal_points,
+			});
+		});
+	}, []);
 
 	return <CircumferenceLayout>
 		<h1 className="text-4xl font-bold mb-1">The Sun's Circumference</h1>
@@ -17,23 +28,23 @@ export default function Sun() {
 		<div className="flex inline-flex justify-between text-gray-500 text-sm mb-6">
 			<div className="mr-4">
 				<span className="mr-1">Decimal Points:</span>
-				<span>{fakeData.decimalPoints}</span>
+				<span>{data.decimalPoints}</span>
 			</div>
 
 			<div className="mr-4">
 				<span className="mr-1">Elapsed Time:</span>
-				<span>{fakeData.elapsedTime} milliseconds</span>
+				<span>{data.elapsedTime} milliseconds</span>
 			</div>
 		</div>
 
-		<div className="flex mb-4">
+		<div className="flex-column mb-4 break-word">
 			<span className="font-bold mr-1">Pi:</span>
-			<p className="flex-row break-word">{fakeData.pi}</p>
+			<p className="break-all max-w-fit">{data.pi}</p>
 		</div>
 
-		<div className="flex">
+		<div className="flex-column break-word">
 			<span className="font-bold mr-1">Circumference:</span>
-			<p className="flex-row break-word">{fakeData.circumference}</p>
+			<p className="break-all max-w-fit">{data.circumference} Kilometers</p>
 		</div>
 	</CircumferenceLayout>;
 }
